d41d4b7d7df31290dd230cc143e3ae1d2d06278f
Vitalii Galkin <galkin@steelkiwi.com>
2017-03-14 11:55:58 +0200

    Load info from angular array

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './components/app.component';
 import { CategoryComponent }  from './components/category.component';
 import { SideNavComponent }  from './components/sidenav.component';
+import { InfoComponent }  from './components/info.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent, CategoryComponent, SideNavComponent ],
+  declarations: [ AppComponent, CategoryComponent, SideNavComponent, InfoComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
