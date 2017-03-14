6f8c999ba25a979abc1b2887efa7db61dc0d6156
Vitalii Galkin <galkin@steelkiwi.com>
2017-03-14 09:59:22 +0200

    Load main navigation from angular array

--- src/app/components/category.component.ts
+++ src/app/components/category.component.ts
@@ -2,8 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'main-nav',
-  templateUrl: './app/components/category.component.html',
+  templateUrl: './app/components/category.component.ng.html',
 })
 export class CategoryComponent  {
+  categories: Category[];
+  constructor(){
+    this.categories = [{id: 1, url: 'test', name: 'Home'}, {id: 1, url: 'test', name: 'Business'}, {
+      id: 1,
+      url: 'test',
+      name: 'Culture'
+    }, {id: 1, url: 'test', name: 'Administrative'}, {id: 1, url: 'test', name: 'About'}]
+  }
+}
 
+interface Category{
+    id: number;
+    name: string;
+    url: string;
 }
